2908a301f021209afd48a3fcca0d2e65fb4e87a5
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-02-03 16:07:11 +0400

    install sass

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,12 @@
+import About from '../pages/about/About'
+
 function App() {
-    return <h1>home page</h1>
+    return (
+        <>
+            <h1>home page</h1>
+            <About />
+        </>
+    )
 }
 
 export default App
